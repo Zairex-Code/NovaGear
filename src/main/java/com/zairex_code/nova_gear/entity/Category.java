624c687b0ff7127b491bb2a7777514a7e6a96905
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-18 09:04:55 -0500

    n capas structure created

---
+++ src/main/java/com/zairex_code/nova_gear/entity/Category.java
@@ -0,0 +1,4 @@
+package com.zairex_code.nova_gear.entity;
+
+public class Category {
+}
---
+++ src/main/java/com/zairex_code/nova_gear/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.zairex_code.nova_gear.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryRepository , Long> {
+    
+}
