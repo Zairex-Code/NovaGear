9c90dcc0e61b3d86afb96c59272ac8c10613592b
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-17 22:35:41 -0500

    my first commit

---
+++ src/main/java/com/zairex_code/nova_gear/NovaGearApplication.java
@@ -0,0 +1,13 @@
+package com.zairex_code.nova_gear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaGearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaGearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zairex_code/nova_gear/NovaGearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zairex_code.nova_gear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaGearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
