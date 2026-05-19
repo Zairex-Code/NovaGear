4e74bfb00dcfd7b8c9ac6d3497a567fcae855657
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-19 11:08:46 -0500

    Category entity created

--- src/main/java/com/zairex_code/nova_gear/entity/Category.java
+++ src/main/java/com/zairex_code/nova_gear/entity/Category.java
@@ -1,4 +1,30 @@
 package com.zairex_code.nova_gear.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Product> products;
+
 }
--- src/main/java/com/zairex_code/nova_gear/entity/Product.java
+++ src/main/java/com/zairex_code/nova_gear/entity/Product.java
@@ -1,4 +1,33 @@
 package com.zairex_code.nova_gear.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 100)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column()
+    private BigDecimal price;
+
+    private Integer stock;
+    private String imageUrl;
+
 }
