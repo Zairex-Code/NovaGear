169ba83e5777e616fad063da0c01b3c0612b98de
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-26 20:28:31 -0500

    Fix/ CategoryJpa fixed

--- src/main/java/com/zairex_code/nova_gear/repository/CategoryRepository.java
+++ src/main/java/com/zairex_code/nova_gear/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.zairex_code.nova_gear.repository;
 
+import com.zairex_code.nova_gear.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<CategoryRepository , Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
