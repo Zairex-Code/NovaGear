354fe73b32bc206bfab5d9b50cc8a81b1361d576
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-29 12:50:25 -0500

    getAllCategories method created

--- src/main/java/com/zairex_code/nova_gear/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/zairex_code/nova_gear/service/impl/CategoryServiceImpl.java
@@ -1,4 +1,38 @@
 package com.zairex_code.nova_gear.service.impl;
 
+import com.zairex_code.nova_gear.dto.category.CategoryRequestDTO;
+import com.zairex_code.nova_gear.dto.category.CategoryResponseDto;
+import com.zairex_code.nova_gear.entity.Category;
+import com.zairex_code.nova_gear.mapper.CategoryMapper;
+import com.zairex_code.nova_gear.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryServiceImpl {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @Override
+     public CategoryResponseDto createCategory(CategoryRequestDTO request){
+        Category category = categoryMapper.toEntity(request);
+
+
+
+     }
+
+    @Override
+    public List<CategoryResponseDto> getAllCategories(){
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::toResponse)
+                .toList();
+    }
+
 }
